1a7a71cbd5a308b7c5a5981f10eea2b6fb3c290f
Josiah <josiah.pilon@gmail.com>
2025-02-27 08:51:05 -0800

    first commit

--- job_scheduler.rb
+++ job_scheduler.rb
@@ -1,15 +1,1 @@
 # rubocop:disable all
-
-class JobScheduler
-  def initialize
-    # Setup background job management here
-  end
-
-  def schedule(time, task)
-    # Implement job scheduling logic
-  end
-
-  def cancel(job_id)
-    # Implement job cancellation logic
-  end
-end
